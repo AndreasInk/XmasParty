70e9437a08aa26fcd86a9bad0a20c803d0a40a12
Andreas Ink <andreasink17@gmail.com>
2021-11-26 16:37:13 -0500

    first commit

---
+++ Shared/XmasPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  XmasPartyApp.swift
+//  Shared
+//
+//  Created by Andreas on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct XmasPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
