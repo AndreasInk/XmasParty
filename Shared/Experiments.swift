58f56957594737825308deb488ed699c802f6c33
Erik Schnell <erikli.schnell@gmail.com>
2021-11-26 23:21:50 +0100

    snowflakes ❅

---
+++ Shared/Experiments.swift
@@ -0,0 +1,38 @@
+//
+//  Experiments.swift
+//  XmasParty
+//
+//  Created by Erik Schnell on 26.11.21.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct Experiments: View {
+    var body: some View {
+        ZStack {
+            Color.red
+            SpriteView(scene: SnowFall(), options: [.allowsTransparency])
+//            SnowFall()
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct Experiments_Previews: PreviewProvider {
+    static var previews: some View {
+        Experiments()
+    }
+}
+
+class SnowFall: SKScene {
+    override func sceneDidLoad() {
+        size = UIScreen.main.bounds.size
+        scaleMode = .resizeFill
+        anchorPoint = CGPoint(x: 0.5, y: 1)
+        backgroundColor = .clear
+        let node = SKEmitterNode(fileNamed: "SnowFall.sks")!
+        addChild(node)
+        node.particlePositionRange.dy = UIScreen.main.bounds.width
+    }
+}
