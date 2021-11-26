1e46b93d79da4e5a7608129a9f029bb5522b351d
Andreas Ink <andreasink17@gmail.com>
2021-11-26 17:36:06 -0500

    Removed duplicate snowfall

--- Shared/Experiments.swift
+++ Shared/Experiments.swift
@@ -25,14 +25,14 @@ struct Experiments_Previews: PreviewProvider {
     }
 }
 
-class SnowFall: SKScene {
-    override func sceneDidLoad() {
-        size = UIScreen.main.bounds.size
-        scaleMode = .resizeFill
-        anchorPoint = CGPoint(x: 0.5, y: 1)
-        backgroundColor = .clear
-        let node = SKEmitterNode(fileNamed: "SnowFall.sks")!
-        addChild(node)
-        node.particlePositionRange.dy = UIScreen.main.bounds.width
-    }
-}
+//class SnowFall: SKScene {
+//    override func sceneDidLoad() {
+//        size = UIScreen.main.bounds.size
+//        scaleMode = .resizeFill
+//        anchorPoint = CGPoint(x: 0.5, y: 1)
+//        backgroundColor = .clear
+//        let node = SKEmitterNode(fileNamed: "SnowFall.sks")!
+//        addChild(node)
+//        node.particlePositionRange.dy = UIScreen.main.bounds.width
+//    }
+//}
